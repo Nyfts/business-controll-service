2a4672a48e906ef48d663f21ac6358e7d36489d4
luan.teixeira <luan.teixeira@dock.tech>
2022-05-27 16:53:50 -0300

    initialize project

---
+++ src/main/java/com/nyfts/businesscontrollservice/BusinessControllServiceApplication.java
@@ -0,0 +1,13 @@
+package com.nyfts.businesscontrollservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusinessControllServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusinessControllServiceApplication.class, args);
+	}
+
+}
